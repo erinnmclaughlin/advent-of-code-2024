c4d543b2e4e778d746465bd7aeb11133981e5e6e
Erin McLaughlin <sa-em5mb@virginia.edu>
2024-12-06 23:55:15 -0500

    more experimenting with day 6 performance

--- runner/Day06.cs
+++ runner/Day06.cs
@@ -8,15 +8,19 @@ public class Day06
 {
     private readonly string[] _fileLines = File.ReadAllLines("day06.txt");
 
-    [Fact, Benchmark]
+    [Fact, Benchmark(Baseline = true)]
     public void PartOne_CSharp() => Assert.Equal(5129, CSharp.Day06.PartOne(_fileLines));
 
     [Fact, Benchmark]
     public void PartOne_CSharp_Optimized() => Assert.Equal(5129, CSharp.Day06Optimized.PartOne(_fileLines));
 
     [Fact, Benchmark]
-    public void PartTwo_CSharp() => Assert.Equal(1888, CSharp.Day06.PartTwo(_fileLines));
+    public void PartOne_CSharp_Optimized_Again() => Assert.Equal(5129, CSharp.Day06MoreOptimized.PartOne(_fileLines));
     
-    [Fact, Benchmark]
-    public void PartTwo_CSharp_Optimized() => Assert.Equal(1888, CSharp.Day06Optimized.PartTwo(_fileLines));
+//    [Fact, Benchmark]
+//    public void PartTwo_CSharp() => Assert.Equal(1888, CSharp.Day06.PartTwo(_fileLines));
+    
+//    [Fact, Benchmark]
+//    public void PartTwo_CSharp_Optimized() => Assert.Equal(1888, CSharp.Day06Optimized.PartTwo(_fileLines));
+    
 }
