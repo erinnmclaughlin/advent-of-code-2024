8d6a42ad566872c6f9885f0fe6d1eae79ed79849
Erin McLaughlin <sa-em5mb@virginia.edu>
2024-12-07 02:51:55 -0500

    day 7 part 2

--- runner/Day07.cs
+++ runner/Day07.cs
@@ -5,8 +5,8 @@ namespace AoC;
 
 public class Day07(ITestOutputHelper output)
 {
-    private readonly string[] _fileLines = File.ReadAllLines("day07.example.txt");
-    //private readonly string[] _fileLines = File.ReadAllLines("day07.txt");
+    //private readonly string[] _fileLines = File.ReadAllLines("day07.example.txt");
+    private readonly string[] _fileLines = File.ReadAllLines("day07.txt");
 
     [Fact]
     public void PartOne()
@@ -31,8 +31,32 @@ public class Day07(ITestOutputHelper output)
         
         output.WriteLine(sum.ToString());
     }
+
+    [Fact]
+    public void PartTwo()
+    {
+        BigInteger sum = 0;
+
+        foreach (var line in _fileLines)
+        {
+            var parts = line.Split(':');
+            var expected = BigInteger.Parse(parts[0]);
+            var numberString = parts[1].Trim();
+
+            foreach (var outcome in GetPossibleOutcomes(numberString, true))
+            {
+                if (outcome == expected)
+                {
+                    sum += outcome;
+                    break;
+                }
+            }
+        }
+        
+        output.WriteLine(sum.ToString());
+    }
     
-    private static IEnumerable<BigInteger> GetPossibleOutcomes(string numberString)
+    private static IEnumerable<BigInteger> GetPossibleOutcomes(string numberString, bool allowConcat = false)
     {
         var numbers = numberString.Split(' ').Select(BigInteger.Parse).ToArray();
 
@@ -42,35 +66,34 @@ public class Day07(ITestOutputHelper output)
         }
         else
         {
-            foreach (var r in GetPossibleValues(numbers[0], numbers[1..]))
+            foreach (var r in GetPossibleValues2(numbers[0], numbers[1..], allowConcat))
                 yield return r;
         }
     }
 
-    private static IEnumerable<BigInteger> GetPossibleValues(BigInteger value, BigInteger[] nextValues)
+    private static IEnumerable<BigInteger> GetPossibleValues2(BigInteger value, BigInteger[] nextValues, bool allowConcat = false)
     {
-        while (true)
+        var nextValue = nextValues[0];
+        
+        var addResult = value + nextValue;
+        var multiplyResult = value * nextValue;
+        var concatResult = BigInteger.Parse($"{value}{nextValue}");
+        
+        if (nextValues.Length == 1)
         {
-            var nextValue = nextValues[0];
-
-            var addResult = value + nextValue;
-            var multiplyResult = value * nextValue;
-
-            if (nextValues.Length == 1)
-            {
-                yield return addResult;
-                yield return multiplyResult;
-            }
-            else
-            {
-                foreach (var r in GetPossibleValues(addResult, nextValues[1..])) yield return r;
-
-                value = multiplyResult;
-                nextValues = nextValues[1..];
-                continue;
-            }
-
-            break;
+            yield return addResult;
+            yield return multiplyResult;
+                
+            if (allowConcat)
+                yield return concatResult;
+        }
+        else
+        {
+            foreach (var r in GetPossibleValues2(addResult, nextValues[1..], allowConcat)) yield return r;
+            foreach (var r in GetPossibleValues2(multiplyResult, nextValues[1..], allowConcat)) yield return r;
+            
+            if (allowConcat)
+                foreach (var r in GetPossibleValues2(concatResult, nextValues[1..], allowConcat)) yield return r;
         }
     }
 }
