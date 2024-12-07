cdac7f343225cc8e794f8862979d893a14ddfada
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-12-07 13:13:23 -0500

    move day 7 into one file

--- csharp/Day07.cs
+++ csharp/Day07.cs
@@ -1,40 +1,34 @@
-namespace AoC.CSharp;
+﻿namespace AoC.CSharp;
 
 public static class Day07
 {
-    public static long PartOne(string[] fileLines)
-    {
-        long sum = 0;
-
-        foreach (var line in fileLines)
+    public static long PartOne(string[] fileLines) => fileLines
+        .AsParallel()
+        .Sum(line =>
         {
             var parts = line.Split(": ");
             var expected = long.Parse(parts[0]);
             var numbers = parts[1].Split(" ").Select(long.Parse).ToArray();
-            sum += GetPossibleOutcomes(numbers, allowConcat: false).FirstOrDefault(o => o == expected);
-        }
+            return GetPossibleOutcomes(expected, numbers, allowConcat: false).FirstOrDefault(o => o == expected);
+        });
 
-        return sum;
-    }
-
-    public static long PartTwo(string[] fileLines)
-    {
-        long sum = 0;
-
-        foreach (var line in fileLines)
+    public static long PartTwo(string[] fileLines) => fileLines
+        .AsParallel()
+        .Sum(line =>
         {
             var parts = line.Split(": ");
             var expected = long.Parse(parts[0]);
             var numbers = parts[1].Split(" ").Select(long.Parse).ToArray();
-            sum += GetPossibleOutcomes(numbers, allowConcat: true).FirstOrDefault(o => o == expected);
-        }
+            return GetPossibleOutcomes(expected, numbers, allowConcat: true).FirstOrDefault(o => o == expected);
+        });
 
-        return sum;
-    }
-
-    private static IEnumerable<long> GetPossibleOutcomes(long[] values, bool allowConcat)
+    private static IEnumerable<long> GetPossibleOutcomes(long maxValue, long[] values, bool allowConcat)
     {
         if (values.Length == 0)
+            yield break;
+
+        // we've exceeded the max value there's no need to keep going
+        if (values[0] > maxValue)
             yield break;
 
         if (values.Length == 1)
@@ -46,19 +40,24 @@ public static class Day07
         var (current, next) = (values[0], values[1]);
         values = values[1..];
         
-        // add
-        values[0] = current + next;
-        foreach (var r in GetPossibleOutcomes(values, allowConcat)) yield return r;
+        // doing this in the order that's most likely to grow faster (so we will break out sooner)
         
         // multiply
         values[0] = current * next;
-        foreach (var r in GetPossibleOutcomes(values, allowConcat)) yield return r;
+        foreach (var r in GetPossibleOutcomes(maxValue, values, allowConcat)) 
+            yield return r;
         
         // concat
-        if (!allowConcat)
-            yield break;
-
-        values[0] = long.Parse($"{current}{next}");
-        foreach (var r in GetPossibleOutcomes(values, allowConcat)) yield return r;
+        if (allowConcat)
+        {
+            values[0] = long.Parse($"{current}{next}");
+            foreach (var r in GetPossibleOutcomes(maxValue, values, allowConcat)) 
+                yield return r;
+        }
+        
+        // add
+        values[0] = current + next;
+        foreach (var r in GetPossibleOutcomes(maxValue, values, allowConcat)) 
+            yield return r;
     }
 }
--- runner/Day07.cs
+++ runner/Day07.cs
@@ -1,26 +1,12 @@
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Order;
-
 namespace AoC;
 
-[Orderer(SummaryOrderPolicy.Declared)]
 public class Day07
 {
-    //private readonly string[] _fileLines = File.ReadAllLines("day07.example.txt");
     private readonly string[] _fileLines = File.ReadAllLines("day07.txt");
 
-    [Fact, Benchmark]
-    public void PartOne_CSharp() => AssertPartOne(CSharp.Day07.PartOne(_fileLines));
+    [Fact]
+    public void PartOne_CSharp() => Assert.Equal(1289579105366, CSharp.Day07.PartOne(_fileLines));
 
-    [Fact, Benchmark]
-    public void PartOne_CSharp_Optimized() => AssertPartOne(CSharp.Day07Optimized.PartOne(_fileLines));
-    
-    [Fact, Benchmark]
-    public void PartTwo_CSharp() => AssertPartTwo(CSharp.Day07.PartTwo(_fileLines));
-    
-    [Fact, Benchmark]
-    public void PartTwo_CSharp_Optimized() => AssertPartTwo(CSharp.Day07Optimized.PartTwo(_fileLines));
-    
-    private static void AssertPartOne(long value) => Assert.Equal(1289579105366, value);
-    private static void AssertPartTwo(long value) => Assert.Equal(92148721834692, value);
+    [Fact]
+    public void PartTwo_CSharp() => Assert.Equal(92148721834692, CSharp.Day07.PartTwo(_fileLines));
 }
