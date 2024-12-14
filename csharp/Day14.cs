3c162c4bbf72dabfe926346241f2882fd4ae11b0
Erin McLaughlin <sa-em5mb@virginia.edu>
2024-12-14 09:19:43 -0500

    day 14 cleanup

--- csharp/Day14.cs
+++ csharp/Day14.cs
@@ -1,6 +1,44 @@
 namespace AoC.CSharp;
 
-public class Day14
+public static class Day14
 {
+    public static long GetQuadrantScore(this IEnumerable<Robot> robots, int numRows, int numCols)
+    {
+        var list = robots.ToList();
+        var q1 = list.Count(x => x.Position.X < numCols / 2 && x.Position.Y < numRows / 2);
+        var q2 = list.Count(x => x.Position.X < numCols / 2 && x.Position.Y > numRows / 2);
+        var q3 = list.Count(x => x.Position.X > numCols / 2 && x.Position.Y < numRows / 2);
+        var q4 = list.Count(x => x.Position.X > numCols / 2 && x.Position.Y > numRows / 2);
+
+        return q1 * q2 * q3 * q4;
+    }
     
+    public sealed class Robot
+    {
+        public (int X, int Y) Position { get; set; }
+        public (int dX, int dY) Velocity { get; set; }
+
+        public void Move(int seconds, int numRows, int numCols)
+        {
+            var x = (Position.X + Velocity.dX * seconds) % numCols;
+            var y = (Position.Y + Velocity.dY * seconds) % numRows;
+
+            if (x < 0) x = numCols + x;
+            if (y < 0) y = numRows + y;
+            
+            Position = (x, y);
+        }
+        
+        public static Robot Parse(string input)
+        {
+            var splitIndex = input.IndexOf(' ');
+            var pos = input[2..splitIndex].Split(',');
+            var vel = input[(splitIndex + 3)..].Split(',');
+            return new Robot
+            {
+                Position = (int.Parse(pos[0]), int.Parse(pos[1])),
+                Velocity = (int.Parse(vel[0]), int.Parse(vel[1]))
+            };
+        }
+    }
 }
--- runner/Day14.cs
+++ runner/Day14.cs
@@ -1,58 +1,23 @@
-using System.Diagnostics;
-using System.Text;
-using Xunit.Abstractions;
+using AoC.CSharp;
 
 namespace AoC;
 
-public class Day14(ITestOutputHelper output)
+public class Day14
 {
     private readonly string[] _input = File.ReadAllLines("day14.txt");
     
     [Fact]
     public void PartOne()
     {
-        var robots = _input.Select(Robot.Parse).ToList();
+        var score = _input.Select(l =>
+        {
+            var robot = CSharp.Day14.Robot.Parse(l);
+            robot.Move(100, 103, 101);
+            return robot;
+        })
+        .GetQuadrantScore(103, 101);
         
-        const int numCols = 101;
-        const int numRows = 103;
-        
-        robots.ForEach(x => x.Move(100, numRows, numCols));
-
-        var q1 = robots.Count(x => x.Position is { X: < numCols / 2, Y: < numRows / 2 });
-        var q2 = robots.Count(x => x.Position is { X: < numCols / 2, Y: > numRows / 2 });
-        var q3 = robots.Count(x => x.Position is { X: > numCols / 2, Y: > numRows / 2 });
-        var q4 = robots.Count(x => x.Position is { X: > numCols / 2, Y: < numRows / 2 });
-
-        var score = q1 * q2 * q3 * q4;
-        output.WriteLine(score.ToString());
+        Assert.Equal(216772608, score);
     }
     
-    private sealed class Robot
-    {
-        public (int X, int Y) Position { get; set; }
-        public (int dX, int dY) Velocity { get; set; }
-
-        public void Move(int seconds, int numRows, int numCols)
-        {
-            var x = (Position.X + Velocity.dX * seconds) % numCols;
-            var y = (Position.Y + Velocity.dY * seconds) % numRows;
-
-            if (x < 0) x = numCols + x;
-            if (y < 0) y = numRows + y;
-            
-            Position = (x, y);
-        }
-        
-        public static Robot Parse(string input)
-        {
-            var splitIndex = input.IndexOf(' ');
-            var pos = input[2..splitIndex].Split(',');
-            var vel = input[(splitIndex + 3)..].Split(',');
-            return new Robot
-            {
-                Position = (int.Parse(pos[0]), int.Parse(pos[1])),
-                Velocity = (int.Parse(vel[0]), int.Parse(vel[1]))
-            };
-        }
-    }
 }
