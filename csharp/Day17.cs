cd2a801dc57caeb3ac98038cce9cb3ee7b1c55c3
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-12-18 21:20:56 -0500

    day 17 part 1

---
+++ csharp/Day17.cs
@@ -0,0 +1,107 @@
+﻿namespace AoC.CSharp;
+
+public static class Day17
+{
+    public sealed class Program(params int[] args)
+    {
+        private readonly int[] _args = args;
+        private int _instructionIndex = 0;
+        
+        public int RegisterA { get; set; }
+        public int RegisterB { get; set; }
+        public int RegisterC { get; set; }
+
+        public int OpCode => _args[_instructionIndex];
+        public int Operand => _args[_instructionIndex + 1];
+        
+        public List<int> Execute()
+        {
+            var output = new List<int>();
+            
+            while (_instructionIndex < _args.Length)
+            {
+                switch (OpCode)
+                {
+                    case 0:
+                        Adv(Operand);
+                        break;
+                    case 1:
+                        Bxl(Operand);
+                        break;
+                    case 2:
+                        Bst(Operand);
+                        break;
+                    case 3:
+                        Jnz(Operand);
+                        break;
+                    case 4:
+                        Bxc(Operand);
+                        break;
+                    case 5:
+                        output.Add(Out(Operand));
+                        break;
+                    case 6:
+                        Bdv(Operand);
+                        break;
+                    case 7:
+                        Cdv(Operand);
+                        break;
+                }
+
+                _instructionIndex += 2;
+            }
+
+            return output;
+        }
+
+        private void Adv(int operand)
+        {
+            RegisterA /= (int)Math.Pow(2, Combo(operand));
+        }
+
+        private void Bst(int operand)
+        {
+            RegisterB = Combo(operand) % 8;
+        }
+
+        private void Bxc(int _)
+        {
+            Bxl(RegisterC);
+        }
+
+        private void Bxl(int operand)
+        {
+            RegisterB ^= operand;
+        }
+
+        private void Jnz(int operand)
+        {
+            if (RegisterA != 0 && _instructionIndex != operand)
+                _instructionIndex = operand - 2;
+        }
+
+        private int Out(int operand)
+        {
+            return Combo(operand) % 8;
+        }
+
+        public void Bdv(int operand)
+        {
+            RegisterB = RegisterA / (int)Math.Pow(2, Combo(operand));
+        }
+
+        public void Cdv(int operand)
+        {
+            RegisterC = RegisterA / (int)Math.Pow(2, Combo(operand));
+        }
+        
+        public int Combo(int opcode) => opcode switch
+        {
+            4 => RegisterA,
+            5 => RegisterB,
+            6 => RegisterC,
+            _ => opcode
+        };
+    }
+    
+}
---
+++ runner/Day17.cs
@@ -0,0 +1,52 @@
+﻿using Xunit.Abstractions;
+
+namespace AoC;
+
+public class Day17(ITestOutputHelper outputHelper)
+{
+    [Fact]
+    public void Test1()
+    {
+        var program = new CSharp.Day17.Program(2,6) { RegisterC = 9 };
+        _ = program.Execute();
+        Assert.Equal(1, program.RegisterB);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var program = new CSharp.Day17.Program(5, 0, 5, 1, 5, 4) { RegisterA = 18 };
+        Assert.Equivalent(new List<int> { 0,1,2 }, program.Execute());
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        var program = new CSharp.Day17.Program(0,1,5,4,3,0) { RegisterA = 2024 };
+        Assert.Equivalent(new List<int> { 4,2,5,6,7,7,7,7,3,1,0 }, program.Execute());
+        Assert.Equal(0, program.RegisterA);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        var program = new CSharp.Day17.Program(1,7) { RegisterB = 29 };
+        program.Execute();
+        Assert.Equal(26, program.RegisterB);
+    }
+
+    [Fact]
+    public void Example()
+    {
+        var program = new CSharp.Day17.Program(0, 1, 5, 4, 3, 0) { RegisterA = 729 };
+        var output = program.Execute();
+        Assert.Equivalent(new List<int>{ 4,6,3,5,6,3,5,2,1,0 }, output);
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var program = new CSharp.Day17.Program(2,4,1,5,7,5,1,6,0,3,4,0,5,5,3,0) { RegisterA = 24847151 };
+        outputHelper.WriteLine(string.Join(',', program.Execute()));
+    }
+}
